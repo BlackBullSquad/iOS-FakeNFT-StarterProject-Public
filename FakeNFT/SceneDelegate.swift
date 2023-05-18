09385e312c86100e546ae90380e1966740a78aca
Maxim Papier <109724501+maxim-papier@users.noreply.github.com>
2023-05-18 12:16:55 +0600

    Refactor: Major Update - Cleaned up the storyboards - Added empty view controllers for main sections: profile, catalog, and cart - Set up navigation bar and tab bar controllers in SceneDelegate - Included new color assets - Added variables for primary, background, and BG colors

--- FakeNFT/SceneDelegate.swift
+++ FakeNFT/SceneDelegate.swift
@@ -2,6 +2,38 @@ import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+        guard let windowScene = (scene as? UIWindowScene) else { return }
 
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {}
+        let profileVC = ProfileVC()
+        let catalogueVC = CatalogueVC()
+        let cartVC = CartVC()
+        
+        let profileNavController = UINavigationController(rootViewController: profileVC)
+        let catalogueNavController = UINavigationController(rootViewController: catalogueVC)
+        let cartNavController = UINavigationController(rootViewController: cartVC)
+    
+        
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [
+            profileNavController,
+            catalogueNavController,
+            cartNavController
+        ]
+        tabBarController.selectedIndex = 1
+        tabBarController.tabBar.backgroundColor = UIColor.lightGray
+        tabBarController.tabBar.tintColor = UIColor.primary
+        
+        profileNavController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.crop.circle.fill"), tag: 0)
+        catalogueNavController.tabBarItem = UITabBarItem(title: "Каталог", image: UIImage(systemName: "rectangle.stack.fill"), tag: 1)
+        cartNavController.tabBarItem = UITabBarItem(title: "Корзина", image: UIImage(systemName: "bag.fill"), tag: 2)
+    
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = tabBarController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
 }
---
+++ FakeNFT/Scenes /CatalogueVC.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class CatalogueVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+        title = "Каталог"
+    }
+    
+}
