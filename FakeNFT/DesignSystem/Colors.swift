0ceb3f6f7481b60928147392a25ab370396f2daa
Maxim Papier <109724501+maxim-papier@users.noreply.github.com>
2023-05-18 17:58:19 +0600

    Reworked the UIColor extension: - Removed unnecessary hexString() method - Grouped colors into two categories Main and Additional using enums.
    Assets:
    - Renamed colors to Asset using camelCase to use the same names in the UIColor extension to avoid entering a rawValue for each color.
    - The color Green appears in the layout in Figma twice, in Main and in Additional. To avoid confusion, I renamed Green from the Additional set to lightGreen. 

--- FakeNFT/DesignSystem/Colors.swift
+++ FakeNFT/DesignSystem/Colors.swift
@@ -1,42 +1,23 @@
 import UIKit
 
 extension UIColor {
-    // Creates color from a hex string
-    convenience init(hexString: String) {
-        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int = UInt64()
-        Scanner(string: hex).scanHexInt64(&int)
-        let alpha, red, green, blue: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (alpha, red, green, blue) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (alpha, red, green, blue) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (alpha, red, green, blue) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (alpha, red, green, blue) = (255, 0, 0, 0)
-        }
-        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: CGFloat(alpha) / 255)
+    
+    enum Main: String {
+        case backround, bg, green, lightGray, primary, red, whiteAlpha50
     }
-
-    // Ниже приведены примеры цветов, настоящие цвета надо взять из фигмы
-
-    // Primary Colors
-    static let primary = UIColor(named: "Primary")
-    static let bg = UIColor(named: "BG")
-    static let lightGray = UIColor(named: "Light-Gray")
     
-    // Background Colors
-    static let background = UIColor(named: "Background")
-
-    // Text Colors
-    static let textPrimary = UIColor.black
-    static let textSecondary = UIColor.gray
-    static let textOnPrimary = UIColor.white
-    static let textOnSecondary = UIColor.black
+    enum Additional: String {
+        case blue, brown, gray, lightGreen, peach, white, yellow
+    }
     
+    static func appColor(_ asset: Main) -> UIColor {
+        guard let color = UIColor(named: asset.rawValue) else { return .black }
+        return color
+    }
     
-    
+    static func appColor(_ asset: Additional) -> UIColor {
+        guard let color = UIColor(named: asset.rawValue) else { return .black }
+        return color
+    }
     
 }
