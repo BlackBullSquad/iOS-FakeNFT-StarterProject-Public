65a036af2f8f16b13a18e112d8d636ee15a327ba
Maxim Papier <109724501+maxim-papier@users.noreply.github.com>
2023-05-20 10:07:05 +0600

    Pull Request #54 Corrections:
    - Revised the method for creating style colors to simplify the interface and make it easier to add new assets

--- FakeNFT/DesignSystem/Colors.swift
+++ FakeNFT/DesignSystem/Colors.swift
@@ -1,23 +1,30 @@
 import UIKit
 
+enum Asset {
+    case main(Main)
+    case additional(Additional)
+
+    var assetName: String {
+        switch self {
+        case let .main(asset):
+            return asset.rawValue
+        case let .additional(asset):
+            return asset.rawValue
+        }
+    }
+}
+
+enum Main: String {
+    case backround, bg, green, lightGray, primary, red, whiteAlpha50
+}
+
+enum Additional: String {
+    case blue, brown, gray, lightGreen, peach, white, yellow
+}
+
 extension UIColor {
-    
-    enum Main: String {
-        case backround, bg, green, lightGray, primary, red, whiteAlpha50
-    }
-    
-    enum Additional: String {
-        case blue, brown, gray, lightGreen, peach, white, yellow
-    }
-    
-    static func appColor(_ asset: Main) -> UIColor {
-        guard let color = UIColor(named: asset.rawValue) else { return .black }
+    static func asset(_ asset: Asset) -> UIColor {
+        guard let color = UIColor(named: asset.assetName) else { return .black }
         return color
     }
-    
-    static func appColor(_ asset: Additional) -> UIColor {
-        guard let color = UIColor(named: asset.rawValue) else { return .black }
-        return color
-    }
-    
 }
