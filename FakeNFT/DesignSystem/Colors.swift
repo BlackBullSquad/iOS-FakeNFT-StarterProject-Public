7db952584847f6f2d1c0d6074b4eb85cb453a1a4
Maxim Papier <109724501+maxim-papier@users.noreply.github.com>
2023-05-20 20:17:20 +0600

    Refactor tabbar (#54)
    * Refactor: Major Update
    - Cleaned up the storyboards
    - Added empty view controllers for main sections: profile, catalog, and cart
    - Set up navigation bar and tab bar controllers in SceneDelegate
    - Included new color assets
    - Added variables for primary, background, and BG colors
    
    * Reworked the UIColor extension:
    - Removed unnecessary hexString() method
    - Grouped colors into two categories Main and Additional using enums.
    
    Assets:
    - Renamed colors to Asset using camelCase to use the same names in the UIColor extension to avoid entering a rawValue for each color.
    - The color Green appears in the layout in Figma twice, in Main and in Additional. To avoid confusion, I renamed Green from the Additional set to lightGreen. 
    
    * Pull Request #54 Corrections:
    - Extracted TabBar and NavBar creation into a separate structure - TabBarControllerBuilder
    
    * Pull Request #54 Corrections:
    
    - Revised the method for creating style colors to simplify the interface and make it easier to add new assets

--- FakeNFT/DesignSystem/Colors.swift
+++ FakeNFT/DesignSystem/Colors.swift
@@ -1,39 +1,30 @@
 import UIKit
 
-extension UIColor {
-    // Creates color from a hex string
-    convenience init(hexString: String) {
-        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int = UInt64()
-        Scanner(string: hex).scanHexInt64(&int)
-        let alpha, red, green, blue: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (alpha, red, green, blue) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (alpha, red, green, blue) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (alpha, red, green, blue) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (alpha, red, green, blue) = (255, 0, 0, 0)
+enum Asset {
+    case main(Main)
+    case additional(Additional)
+
+    var assetName: String {
+        switch self {
+        case let .main(asset):
+            return asset.rawValue
+        case let .additional(asset):
+            return asset.rawValue
         }
-        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: CGFloat(alpha) / 255)
     }
+}
 
-    // Ниже приведены примеры цветов, настоящие цвета надо взять из фигмы
+enum Main: String {
+    case backround, bg, green, lightGray, primary, red, whiteAlpha50
+}
 
-    // Primary Colors
-    static let primary = UIColor(red: 0 / 255, green: 122 / 255, blue: 255 / 255, alpha: 1.0)
+enum Additional: String {
+    case blue, brown, gray, lightGreen, peach, white, yellow
+}
 
-    // Secondary Colors
-    static let secondary = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1.0)
-
-    // Background Colors
-    static let background = UIColor.white
-
-    // Text Colors
-    static let textPrimary = UIColor.black
-    static let textSecondary = UIColor.gray
-    static let textOnPrimary = UIColor.white
-    static let textOnSecondary = UIColor.black
+extension UIColor {
+    static func asset(_ asset: Asset) -> UIColor {
+        guard let color = UIColor(named: asset.assetName) else { return .black }
+        return color
+    }
 }
---
+++ FakeNFT/Scenes/ProfileVC.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ProfileVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        title = "Профиль"
+    }
+}
