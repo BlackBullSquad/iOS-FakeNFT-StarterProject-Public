f525f18db7a6610a789484d46d304882f36eccef
Andrei Chenchik <andrei@chenchik.me>
2023-06-07 10:17:38 +0200

    Fix network layer and add tests (#85)

--- FakeNFT/Services/API/FakeAPI/FakeAPIRequest.swift
+++ FakeNFT/Services/API/FakeAPI/FakeAPIRequest.swift
@@ -21,43 +21,46 @@ enum FakeAPIRequest {
     case getUser(id: Int)
 }
 
-extension FakeAPIRequest: NetworkRequest {
-    var endpoint: URL? { URL(string: urlString) }
-    var queryParameters: [String: String]? { requestParameters }
-    var httpMethod: HttpMethod { requestMethod }
-}
+ extension FakeAPIRequest {
+    var request: NetworkRequest {
+        .init(endpoint: URL(string: urlString),
+                       queryParameters: nil,
+                       payload: requestPayload,
+                       httpMethod: requestMethod)
+    }
+ }
 
 private extension FakeAPIRequest {
-    static let baseURLString = "https://64411f3a792fe886a89efa72.mockapi.io"
-
     var urlString: String {
+        let baseURLString = "https://64411f3a792fe886a89efa72.mockapi.io"
+
         switch self {
         case .getCollections:
-            return Self.baseURLString + "/api/v1/collections"
+            return baseURLString + "/api/v1/collections"
         case let .getCollection(id):
-            return Self.baseURLString + "/api/v1/collections/\(id)"
+            return baseURLString + "/api/v1/collections/\(id)"
         case .getNfts:
-            return Self.baseURLString + "/api/v1/nft"
+            return baseURLString + "/api/v1/nft"
         case let .getNft(id):
-            return Self.baseURLString + "/api/v1/nft/\(id)"
+            return baseURLString + "/api/v1/nft/\(id)"
         case let .getCurrency(id):
-            return Self.baseURLString + "/api/v1/currencies/\(id)"
+            return baseURLString + "/api/v1/currencies/\(id)"
         case .getCurrencies:
-            return Self.baseURLString + "/api/v1/currencies"
+            return baseURLString + "/api/v1/currencies"
         case let .getOrder(id):
-            return Self.baseURLString + "/api/v1/orders/\(id)"
+            return baseURLString + "/api/v1/orders/\(id)"
         case let .payOrder(id, currencyId):
-            return Self.baseURLString + "/api/v1/orders/\(id)/payment/\(currencyId)"
+            return baseURLString + "/api/v1/orders/\(id)/payment/\(currencyId)"
         case let .updateOrder(id, _):
-            return Self.baseURLString + "/api/v1/orders/\(id)"
+            return baseURLString + "/api/v1/orders/\(id)"
         case let .getProfile(id):
-            return Self.baseURLString + "/api/v1/profile/\(id)"
+            return baseURLString + "/api/v1/profile/\(id)"
         case let .updateProfile(id, _, _, _, _):
-            return Self.baseURLString + "/api/v1/profile/\(id)"
+            return baseURLString + "/api/v1/profile/\(id)"
         case .getUsers:
-            return Self.baseURLString + "/api/v1/users"
+            return baseURLString + "/api/v1/users"
         case let .getUser(id):
-            return Self.baseURLString + "/api/v1/users/\(id)"
+            return baseURLString + "/api/v1/users/\(id)"
         }
     }
 }
@@ -74,19 +77,54 @@ extension FakeAPIRequest {
 }
 
 extension FakeAPIRequest {
-    var requestParameters: [String: String]? {
+    var requestPayload: Encodable? {
         switch self {
         case let .updateOrder(_, nfts):
-            return ["nfts": nfts.map(String.init).joined(separator: ",")]
+            return UpdateOrderDTO(nfts: nfts)
+
         case let .updateProfile(_, name, description, website, likes):
-            return [
-                "name": name,
-                "description": description,
-                "website": website.absoluteString,
-                "likes": likes.map(String.init).joined(separator: ",")
-            ]
+            return UpdateProfileDTO(name: name,
+                                    description: description,
+                                    website: website,
+                                    likes: likes)
+
         default:
             return nil
         }
     }
 }
+
+// extension FakeAPIRequest {
+//    func getNetworkRequest<Payload: Encodable>() -> NetworkRequest<Payload> {
+//        switch self {
+//        case let .updateOrder(_, nfts):
+//            let payload = UpdateOrderDTO(nfts: nfts)
+//
+//            return NetworkRequest<Payload>(endpoint: URL(string: urlString),
+//                                                  queryParameters: nil,
+//                                                  payload: payload,
+//                                                  httpMethod: requestMethod)
+//
+//        case let .updateProfile(_, name, description, website, likes):
+//            let payload =  UpdateProfileDTO(name: name,
+//                                            description: description,
+//                                            website: website,
+//                                            likes: likes)
+//
+//            return NetworkRequest<Payload>(endpoint: URL(string: urlString),
+//                                                    queryParameters: nil,
+//                                                    payload: payload,
+//                                                    httpMethod: requestMethod)
+//
+//        default:
+//            return NetworkRequest(endpoint: URL(string: urlString),
+//                                  queryParameters: nil,
+//                                  httpMethod: requestMethod)
+//        }
+//    }
+// }
+
+//        NetworkRequest(endpoint: URL(string: urlString),
+//                       queryParameters: nil,
+//                       payload: requestPayload,
+//                       httpMethod: requestMethod)
